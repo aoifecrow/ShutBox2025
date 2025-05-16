70fc4fed1ed15a912a5490894e5af7002bec8812
aoife <aoife@10.125.80.26>
2025-05-16 12:56:58 -0400

    adding comments

--- CrowShut2/src/crow/aoife/GUIShutDriver.java
+++ CrowShut2/src/crow/aoife/GUIShutDriver.java
@@ -12,32 +12,39 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * Shut the Box parlor game Interface
+ * 
+ * @author A.Crow
+ * @version 1.0
+ */
 public class GUIShutDriver extends Application {
 
+	//Global Variables 
 	Die d1 = new Die();
 	Die d2 = new Die();
 	int player1Score = 0;
 	int player2Score = 0;
 	int currentPlayer = 1;
 	int currentRound = 1;
-	boolean gameOver = false;
 	boolean first789 = true;
+	
 	@Override
 	public void start(Stage stage) throws Exception {
+		
 		//set up main scene
-		VBox root = new VBox(10); 
-		
-		
-		root.setAlignment(Pos.CENTER);
-		Button[] tileBtns = new Button[9];
-		Label gameName = new Label("Shut The Box");
-		gameName.setStyle("-fx-font-size: 40px");
-		Label player = new Label("Player "+currentPlayer);
-		player.setStyle("-fx-font-size: 24px");
-		Label round = new Label("Current Round:"+currentRound);
-		round.setStyle("-fx-font-size: 18px");
-		Label p1ScoreLabel = new Label("Player 1 Score : " + player1Score);
-		Label p2ScoreLabel = new Label("Player 2 Score : " + player2Score);
+		VBox root = new VBox(10); //plane for everything	
+		root.setAlignment(Pos.CENTER); //centered
+		Button[] tileBtns = new Button[9]; //creates array for buttons
+		Label gameName = new Label("Shut The Box"); //create title
+		gameName.setStyle("-fx-font-size: 40px"); //set style
+		Label player = new Label("Player "+currentPlayer); //title for current player
+		player.setStyle("-fx-font-size: 24px");//set style
+		Label round = new Label("Current Round:"+currentRound); //title for current round
+		round.setStyle("-fx-font-size: 18px"); //set style
+		Label p1ScoreLabel = new Label("Player 1 Score : " + player1Score);// title for player 1 score
+		Label p2ScoreLabel = new Label("Player 2 Score : " + player2Score);// title for player 2 score
+		//set styles
 		p1ScoreLabel.setStyle("-fx-font-size: 16px");
 		p2ScoreLabel.setStyle("-fx-font-size: 16px");
 		
@@ -47,42 +54,55 @@ public class GUIShutDriver extends Application {
 		over.setAlignment(Pos.CENTER);
 		over.getChildren().add(messageOver);
 		
+		//create buttons for each tile and put in array 
 		for (int i=0; i<tileBtns.length; i++) {
 			Button button= new Button (String.valueOf(i+1));
-			button.setStyle("-fx-background-color:#FFFFFF");
+			button.setStyle("-fx-background-color:#FFFFFF"); //set unselected colour
 			tileBtns[i] = button;
 		}
 		
-		//Creat the markers
-		Tile[] markers = new Tile[9];
+		Tile[] markers = new Tile[9];//array list for tiles
+		//create tiles for each number and put in array
 		for (int i=0; i<tileBtns.length; i++) {
 			markers[i] = new Tile(i+1);
 		}
 		
-		HBox btnBox = new HBox(10);
-		btnBox.setAlignment(Pos.CENTER);
+		HBox btnBox = new HBox(10); //horizontal plane for buttons to be sideways
+		btnBox.setAlignment(Pos.CENTER); //centered
+		
+		// change state of each button
 		for (Button b: tileBtns) {
-			btnBox.getChildren().add(b);
+			btnBox.getChildren().add(b); //add each button to plane
+			
+			//change state of button when pressed
 			b.setOnAction(e-> {
 				int number = Integer.valueOf(b.getText());
+				
+				//checks if the tile is not already down
 				if (!markers[(number-1)].isDown()) {
+					
+					//checks if tile is not already selected
 					if(!markers[(number-1)].isSelected()) {
 						//set the color for selection
 						b.setStyle("-fx-background-color: #90EE90");
-						markers[(number-1)].select();
+						markers[(number-1)].select(); //select tile
 					}
+					//if buttons are selected then deselect
 					else {
-						//return colour back to unselected
+						//return colour back to deselected
 						b.setStyle("-fx-background-color: #FFFFFF");
-						markers[(number-1)].deselect();
+						markers[(number-1)].deselect(); //deselect tile
 					}
 				}
-		});
+		}); // end of button click
+			
 		}// end of event handler for tile selection buttons
 		
+		// Create button that finalizes selected tiles 
 		Button lockIn = new Button("Lock IN");
-		lockIn.setDisable(true);
+		lockIn.setDisable(true); //disable lockIn
 		
+		//set up for roll result
 		HBox result = new HBox(2);
 		Label rollStatement = new Label("You rolled:");
 		Label dieResult = new Label("");
@@ -90,50 +110,66 @@ public class GUIShutDriver extends Application {
 		dieResult.setStyle("-fx-font-size: 16 px");
 		Button btnRoll = new Button("Roll 2 Die");
 	
-		Button roundOver = new Button("Done");
+		Button roundOver = new Button("Done"); //finished button
 		
 		
-		Scene scene2 = new Scene(over,500,500);
+		Scene scene2 = new Scene(over,500,500); //set up game over scene
+		
+		//Round over
 		roundOver.setOnAction(e->{
-			int roundSum =0; 
+			int roundSum =0; //sum for the round
+			
+			//counts the sum of tiles that are still up
 			for(Tile t: markers) {
 				if(!t.isDown()) {
 				roundSum+=t.getValue();
 				}
 			}
+			
+			//adds score to player 1
 			if (currentPlayer == 1) {
 				player1Score += roundSum;
 				
 			}
+			//adds score to player 2
 			else {
 				player2Score +=roundSum;
 			}
+			
+			//reset tiles
 			for (int i =0; i<tileBtns.length; i++ ) {
 				tileBtns[i].setStyle("-fx-background-color: #FFFFFF");
 				markers[i].open();
 				tileBtns[i].setDisable(false);
 			} 
+			
+			// if the currentRound is under 3
 			if (currentRound<3) {
-				currentRound++;
+				currentRound++; //adds to current round
 				
 			}
+			// if the last round was player 1 round 3 then switch to player 2
 			else if (currentPlayer==1){
 				currentRound = 1;
 				currentPlayer =2;
 			}
+			// if it's the last round, find winner
 			else if (currentPlayer==2) {
-				gameOver = true;
+				
+				//finds lowest score and winner
 				if (player1Score <player2Score) {
-					messageOver.setText("Player 1 Won! Score: "+player1Score);
+					messageOver.setText("Player 1 Won! Score: "+player1Score);//player 1 win message
 				}
 				else if (player2Score <player1Score) {
-					messageOver.setText("Player 2 Won! Score: "+player2Score);
+					messageOver.setText("Player 2 Won! Score: "+player2Score);//player 2 win message
 				}
 				else {
-					messageOver.setText("Tie! Scores: "+player1Score);
+					messageOver.setText("Tie! Scores: "+player1Score); //tie message
 				}
-				stage.setScene(scene2);
+				stage.setScene(scene2); //game over screen
 			}
+			
+			//reset all text labels and buttons
 			player.setText("Player "+currentPlayer);
 			round.setText("Current Round:"+currentRound);
 			p1ScoreLabel.setText("Player 1 Score:" + player1Score);
@@ -145,21 +181,25 @@ public class GUIShutDriver extends Application {
 			lockIn.setDisable(true);
 			});
 		
+		//roll die
 		btnRoll.setOnAction(e-> {
-			int sumDie = 0;
+			int sumDie = 0; //sum of die
+			
+			//if 7,8,9 tiles are down only roll 1 die
 			if (markers[6].isDown() && markers[7].isDown() && markers[8].isDown()) {
-				sumDie = d1.roll();
+				sumDie = d1.roll(); //roll 1 die
 			}
 			else {
-				sumDie = d1.roll() +d2.roll();
+				sumDie = d1.roll() +d2.roll(); //roll 2 die
 			}
-			dieResult.setText(String.valueOf(sumDie));
-			btnRoll.setDisable(true);
+			dieResult.setText(String.valueOf(sumDie)); //display sum of die
+			btnRoll.setDisable(true); //turn off roll ability
+			
 			//enable potential lock in 
 			lockIn.setDisable(false);
 		});
 		
-
+		
 		lockIn.setOnAction( e-> {
 			//determine the sum of the selected tiles
 			int sum = 0;
--- CrowShut2/src/crow/aoife/Tile.java
+++ CrowShut2/src/crow/aoife/Tile.java
@@ -1,41 +1,84 @@
 package crow.aoife;
 
-
+/**
+ * Class to represent a single Tile
+ * @author A.Crow
+ * @version 1.0
+ */
 public class Tile {
 	private int value;
 	private boolean isDown;
 	private boolean selected;
+	
+	/**
+	 * Constructor for a Tile - default to down and not selected
+	 * @param v
+	 */
 	public Tile(int v) {
 		value = v;
 		isDown = false;
 		selected = false;
 	}
 	
+	/**
+	 * Sets the Tile as shut
+	 */
 	public void shut() {
 		isDown=true;
 	}
+	
+	/**
+	 * Sets the Tile as open
+	 */
 	public void open() {
 		isDown = false;
 	}
 	
+	/**
+	 * Sets the Tile as down
+	 * @return
+	 */
 	public boolean isDown() {
 		return isDown;
 	}
+	
+	/**
+	 * Sets the Tile as selected
+	 */
 	public void select() {
 		selected = true;
 	}
+	
+	/**
+	 * Shows wether the state of selection 
+	 * @return
+	 */
 	public boolean isSelected() {
 		return selected;
 	}
+	
+	/**
+	 * Sets the Tile as unselected
+	 */
 	public void deselect() {
 		selected = false;
 	}
+	
+	/**
+	 * Shows the value of the Tile
+	 * @return
+	 */
 	public int getValue() {
 		return value;
 	}
+	/**
+	 * Create a string representation of the object Tile
+	 */
 	@Override 
 	public String toString() {
 		String state ="";
+		
+		//Checks the state of isDown
 		if (isDown) {
 			state ="D";
 		}
